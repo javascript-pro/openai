9da62f5ada58f8b90d5aeb40895fbf3c88f21236
listingslab <listingslab@gmail.com>
2024-12-23 20:59:33 +0000

    brick wall begs monorepo

--- components/sidebar-history.tsx
+++ components/sidebar-history.tsx
@@ -188,7 +188,7 @@ export function SidebarHistory({ user }: { user: User | undefined }) {
     setShowDeleteDialog(false);
 
     if (deleteId === id) {
-      router.push('/');
+      router.push('/chat');
     }
   };
 
